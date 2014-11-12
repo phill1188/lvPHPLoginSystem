ae81c0568b6d644c8640af0b9f19c6b1129f2bfd
Dimitar Georgiev <dmtr_georgiev@mail.bg>
2014-11-11 23:45:56 -0800

    DB.php updated

--- classes/DB.php
+++ classes/DB.php
@@ -5,4 +5,23 @@ class DB{
 	        $_error = false,
 			$_results,
 			$_count = 0;
+	 
+	 
+	 //uses singleton pattern to instantiate DB objects
+	 private function __construct(){
+	     try{
+		     $this->_pdo = new PDO('mysql:host='.Config::get('mysql/host').';dbname='.Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
+		 }//try
+		 catch(PDOException $e){
+		     exit($e->getMessage());
+		 }//catch
+	 }//__construct()
+	 
+	 
+	 public static function getInstance(){
+	     if(!isset(selft::$_instance)){
+		     self::$_instance = new DB();
+		 }//if
+		 return self::$_instance;
+	 }//getInstance()
 }//class DB
